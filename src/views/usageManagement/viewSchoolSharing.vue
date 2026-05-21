24e83c6160f02ee44e658a9d5cd08ef695e4d17f
yangxh <1164362821@qq.com>
2026-05-21 16:20:08 +0800

    fix：修改数据取值来源，修改显示文字

--- src/views/usageManagement/viewSchoolSharing.vue
+++ src/views/usageManagement/viewSchoolSharing.vue
@@ -553,16 +553,16 @@
                         <div class="dialog-item tip-item">
                             <p></p>
                             <div class="info-item">
-                                AI课堂分析待分配：{{ $comjs.formatValue(frequency[0]?.remainNum) }}次，
+                                AI课堂分析剩余：{{ $comjs.formatValue(curRow['aiClassRemain']) }}次，
                             </div>
                             <div class="info-item">
-                                大单元及学情分析待分配：{{ $comjs.formatValue(frequency[2]?.remainNum) }}次，
+                                大单元及学情分析剩余：{{ $comjs.formatValue(curRow['bigUnitRemain']) }}次，
                             </div>
                             <div class="info-item">
-                                赛课辅导待分配：{{ $comjs.formatValue(frequency[1]?.remainNum) }}次，
+                                赛课辅导剩余：{{ $comjs.formatValue(curRow['contestRemain']) }}次，
                             </div>
                             <div class="info-item">
-                                AI课前指导待分配：{{ $comjs.formatValue(frequency[3]?.remainNum) }}次
+                                AI课前指导剩余：{{ $comjs.formatValue(curRow['preClassRemain']) }}次
                             </div>
                         </div>
                         <div class="dialog-item">

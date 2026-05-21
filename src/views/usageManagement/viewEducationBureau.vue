6019cbdd93a83a4c19773b53b3fab146040f16c5
yangxh <1164362821@qq.com>
2026-05-21 16:19:11 +0800

    fix：新增“操作类型”列

--- src/views/usageManagement/viewEducationBureau.vue
+++ src/views/usageManagement/viewEducationBureau.vue
@@ -262,6 +262,8 @@
                                     }}</span>
                                 </template>
                             </el-table-column>
+                            <el-table-column align="center" prop="operateTypeName" label="操作类型" width="120px">
+                            </el-table-column>
                             <el-table-column align="center" prop="remark" label="备注" width="238px">
                                 <template #default="scoped">
                                     <el-tooltip
